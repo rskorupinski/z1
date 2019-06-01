14d0628eb1e57a24bafc25f6bd02cdff26531172
rskorupinski <rskorupinski@poczta.onet.pl>
2019-06-01 12:36:46 +0200

    komentarz2

--- Z1.cpp
+++ Z1.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello Poland!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hi Poland!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
